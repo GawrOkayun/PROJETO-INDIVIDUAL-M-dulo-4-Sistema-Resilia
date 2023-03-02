abd1a51194e2c5c2d163d4c08d215b26fac1a4dd
GawrOkayun <115713180+GawrOkayun@users.noreply.github.com>
2023-03-01 21:31:24 -0300

    Add files via upload

---
+++ SQL/SistemaResilia.sql
@@ -0,0 +1,40 @@
+CREATE TABLE `cursos` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100),
+  `unidade` varchar(255),
+  `endereço` int NOT NULL
+  );
+CREATE TABLE `turmas` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `codigo` int(3),
+  `nome` varchar(100),
+  `id_turma` int NOT NULL
+);
+CREATE TABLE `alunos` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100),
+  `cpf` int(11),
+  `telefone` int(12),
+  `email` varchar(100),
+  `sexo` enum('feminino','masculino','outro') 
+);
+CREATE TABLE `professor` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `cpf` int(11) NOT NULL,
+  `email` varchar(255),
+  `telefone` int(11),
+  `sexo` enum('feminino','masculino','outro')
+  );
+  CREATE TABLE `monitores` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `cpf` int(11) NOT NULL,
+  `email` varchar(255),
+  `telefone` int(11),
+  `sexo` enum('feminino','masculino','outro')
+);
+ALTER TABLE `alunos` ADD FOREIGN KEY (`id_turmas`) REFERENCES `turmas` (`id`);
+ALTER TABLE `professor` ADD FOREIGN KEY (`id_turmas`) REFERENCES `turmas` (`id`);
+ALTER TABLE `turmas` ADD FOREIGN KEY (`id_cursos`) REFERENCES `cursos` (`id`);
+ALTER TABLE `monitores` ADD FOREIGN KEY (`id_turmas`) REFERENCES `turmas` (`id`);
